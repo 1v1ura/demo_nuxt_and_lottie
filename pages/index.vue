cbe413dda3e31c38365b1cc59efa0b4c08ac12cc
Takuma Miura <21156330+tkm-mur@users.noreply.github.com>
2019-10-12 21:27:01 +0900

    デモ完成ファイルを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,24 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        demo_nuxt_and_lottie
-      </h1>
-      <h2 class="subtitle">
-        My supreme Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <lottie :options="defaultOptions" v-on:animCreated="handleAnimation"/>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Lottie from '~/components/lottie.vue';
+import * as animationData from '~/assets/SaCSS.json';
 
 export default {
-  components: {
-    Logo
+  components: { Lottie },
+  data() {
+    return {
+      defaultOptions: { animationData },
+    }
+  },
+  methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim;
+    }
   }
 }
 </script>
@@ -41,32 +26,12 @@ export default {
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  background: center / cover no-repeat url("~assets/sapporo.jpg");
+  overflow: hidden;
 }
 </style>
